42344c2addf9dac0da88cef8e8dd444db6daaa6d
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-08-07 11:55:39 +0200

    Listing all Tweet objects in home template

--- src/twitter/views.py
+++ src/twitter/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from twitter.models import Tweet
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "twitter/home.html")
+        tweets = Tweet.objects.all()
+        return render(request, "twitter/home.html", {
+            "tweets": tweets
+        })
