ae96f03b0a6cc768b1324d59f60c3744dbe094c2
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-08-07 11:33:53 +0200

    Created model Tweet and runned migrations

--- src/twitter/models.py
+++ src/twitter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Tweet(models.Model):
+    content = models.TextField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
