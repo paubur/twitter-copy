87b5cae2a46a753d46875e3ba66bd2d519a17b6f
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-08-07 11:23:55 +0200

    Fixed problem

--- src/twitter/tests.py
+++ src/twitter/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from twitter.views import HomeView
 
-# Create your tests here.
+
+class TestHomeView(TestCase):
+
+    def test_get_method_returns_status_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_method_renders_appropriate_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "twitter/home.html")
